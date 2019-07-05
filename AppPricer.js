989da2cfa1cd213eecd6798dab1b7d6de4905f20
Z-Turns <greedyage@hotmail.com>
2019-07-05 04:38:34 -0700

    Add files via upload

---
+++ AppPricer.js
@@ -0,0 +1,55 @@
+/*
+App Pricer for Surge by Neurogram
+
+ - App 价格监控
+ 
+使用说明：https://www.notion.so/neurogram/App-Pricer-bb7f6877caa24e8584cc63bddf6e555a
+
+关于作者
+Telegram: Neurogram
+GitHub: Neurogram-R
+*/
+
+const region = "cn"
+const appIds = ["1312014438", "1423330822", "1085978097"]
+
+var cacheData = $persistentStore.read()
+if (!cacheData) {
+    cacheData = {}
+} else {
+    cacheData = JSON.parse(cacheData)
+}
+
+$httpClient.post('https://itunes.apple.com/lookup?id=' + appIds + "&country=" + region, function (error, response, data) {
+    if (error) {
+        console.log(error);
+        $notification.post("App Pricer", "获取价格失败")
+        $done()
+    } else {
+        let appData = JSON.parse(data).results
+        let priceChanged = ""
+        let newAppAdded = ""
+        for (var i = 0; i < appData.length; i++) {
+            if (cacheData[appData[i].trackId]) {
+                if (appData[i].formattedPrice != cacheData[appData[i].trackId].price) {
+                    priceChanged = priceChanged + "🏷 " + appData[i].trackName + "  " + cacheData[appData[i].trackId].price + " → " + appData[i].formattedPrice + "\n"
+                    cacheData[appData[i].trackId].price = appData[i].formattedPrice
+                }
+            } else {
+                newAppAdded = newAppAdded + "🏷 " + appData[i].trackName + "  " + appData[i].formattedPrice + "\n"
+                cacheData[appData[i].trackId] = {
+                    name: appData[i].trackName,
+                    price: appData[i].formattedPrice
+                }
+            }
+        }
+        if (priceChanged) {
+            $notification.post("Price Changed", "", priceChanged)
+        }
+        if (newAppAdded) {
+            $notification.post("New Apps Added", "", newAppAdded)
+        }
+        $persistentStore.write(JSON.stringify(cacheData))
+        $done()
+    }
+})
